1cd4bc9ca0d7b48d967d8d06fa2a22c3bbb75648
CaimingLiao <CaiMingLiao@sf-express.com>
2018-08-15 11:25:25 +0800

    first commit

---
+++ chinaAreaList.d.ts
@@ -0,0 +1,8 @@
+export declare const chinaAreaList: {
+    "label": string;
+    "value": string;
+    "children": {
+        "label": string;
+        "value": string;
+    }[];
+}[];
---
+++ index.d.ts
@@ -0,0 +1,1 @@
+export { chinaAreaList } from './chinaAreaList';
